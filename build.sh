33557fdbadf64dbb692da30f5d05674a73a60089
Robert Zenz <Robert.Zenz@bonsaimind.org>
2018-10-14 17:37:19 +0200

    #5 CSS is now being minified.

--- build.sh
+++ build.sh
@@ -23,18 +23,33 @@ if [ ! -d "$OUT" ]; then
 	mkdir "$OUT"
 fi
 
+# Create the needed style file.
+styleFile="$OUT/index.css"
+
+# Create the css file to be included in the header.
+echo -n "<style>" > "$OUT/index.css"
+cat "$TEMPLATES/index.css" \
+	| tr --delete "\n\t" \
+	| sed --regexp-extended "s/ ?([:,>{]) ?/\1/g" \
+	>> "$OUT/index.css"
+echo -n "</style>" >> "$OUT/index.css"
+
 # Run Pandoc.
 $TOOLCHAIN/pandoc-linux-x64 \
 	--from=markdown-smart+ascii_identifiers \
 	--to=html \
 	--output="$OUT/index.html" \
 	--eol=lf \
-	--template="$TEMPLATES/index.template" \
+	--template="$TEMPLATES/index.html" \
 	--lua-filter="$TOOLCHAIN/filter.lua" \
+	--include-in-header="$OUT/index.css" \
 	--variable="DATE:$(date +%Y-%m-%d\ %H:%M:%S\ %::z)" \
 	--variable="GIT_REPO:https://gitlab.com/RobertZenz/bonsaimind.org" \
 	--variable="GIT_COMMIT:$(git rev-parse HEAD 2> /dev/null)" \
 	"$CONTENT/index.markdown"
+
+# Cleanup
+rm "$OUT/index.css"
 
 # Copy the resource files.
 cp -f "$CONTENT/avatar.png" "$OUT/"
